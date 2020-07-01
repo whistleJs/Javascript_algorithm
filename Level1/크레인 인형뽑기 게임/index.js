a42a698b819ccfde0b9667a9e339565faf5bed56
whistleJs <whistle_js@naver.com>
2020-07-01 13:51:31 +0900

    Level 1 - [2019 카카오 개발자 인턴쉽] 크레인 인형뽑기 게임

---
+++ Level1/크레인 인형뽑기 게임/index.js
@@ -0,0 +1,28 @@
+function solution(board, moves) {
+  let result = 0;
+  let stack = [];
+  
+  for (let i = 0 ; i < moves.length ; i++) {
+      let move = moves[i];
+      
+      for (let j = 0 ; j < board[0].length ; j++) {
+          let item = board[j][move - 1];
+          
+          if (item > 0) {
+              stack.push(item);
+              board[j][move - 1] = 0;
+              break;
+          }
+      }
+  }
+  
+  for (let i = 0 ; i < stack.length - 1 ; i++) {
+      if (stack[i] == stack[i + 1]) {
+          stack.splice(i, 2);
+          result += 2;
+          i -= 2;
+      }
+  }
+  
+  return result;
+}
