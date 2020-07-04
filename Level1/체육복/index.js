c1fdd218196d49a187f80f673a15977d5f7c02dc
whistleJs <42595869+whistleJs@users.noreply.github.com>
2020-07-04 14:07:19 +0900

    Level 1 - [탐욕법(Greedy)] 체육복

---
+++ Level1/체육복/index.js
@@ -0,0 +1,25 @@
+function solution(n, lost, reserve) {
+    let result = n;
+
+    for (let i = 0; i < lost.length; i++) {
+        for (let j = 0; j < reserve.length; j++) {
+            if (lost[i] === reserve[j]) {
+                result++;
+                reserve[j] = lost[i] = -1;
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < lost.length; i++) {
+        for (let j = 0; j < reserve.length; j++) {
+            if (lost[i] === reserve[j] - 1 || lost[i] === reserve[j] + 1) {
+                result++;
+                reserve[j] = -1;
+                break;
+            }
+        }
+    }
+    
+    return result - lost.length;
+}
