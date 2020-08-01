a61d10bf89cf901108eae8d6f46cfa9cab80bb4d
whistleJs <whistle_js@naver.com>
2020-08-01 21:07:51 +0900

    Level 1 - [연습문제] 약수의 합

---
+++ Level1/약수의 합/index.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let filter = 0;
+    
+    for (let i = 1 ; i <= Math.sqrt(n) ; i++) {
+        if (n / i == ~~(n / i)) {
+            if (n / i == i) {
+                filter += i;
+            } else {
+                filter += (n / i) + i;
+            }
+        }
+    }
+    
+    return filter;
+}
